372728f059c63434dfbe1739df4040094386e847
tomsmith01 <thomas.smith2110@gmail.com>
2022-03-28 23:00:15 +0100

    Country selection, dictionary added

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
+import random
+
+#store countries and information in dictionary
+countries_dict = {}
+
+#populate the dictionary with country and population key pairs
 def get_countries_from_file():
     with open('countries.txt') as f:
         lines = f.readlines()
+        for line in lines:
+            parts = line.split(' | ')
+            population = get_population_from_line(parts[len(parts) - 1])
+            countries_dict[parts[1]] = population
+            
+
+#get a random country from the dictionary
+def get_random_country():
+    #TODO implement here
+    country = random.choice(list(countries_dict.keys()))
+    return country
 
 #get an integer value of the country population
-def get_population_from_text_line(line):
+def get_population_from_line(line):
     words = line.split()
+    pop = words[0].translate({ord(','): None})
     if 'million' in line:
-        return words[0]*1000000
-    return words*1
+        removed_dec_pop = float(pop)*100
+        return int(removed_dec_pop)*10000
+    return int(pop)
+
